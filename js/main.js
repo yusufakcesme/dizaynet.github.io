026a4076de0f4df0aef0954311a31cf8c764082d
yusuf akcesme <yusufakcesme0@gmail.com>
2020-03-17 18:58:42 +0300

    dizaynet

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+/*
+function openDrop(e) {
+	var item = e.childNodes[3];
+
+	if (item.style.opacity === "1" && item.style.pointerEvents === "all") {
+		item.style.opacity = "0";
+		item.style.pointerEvents = "none";
+	} else {
+		item.style.opacity = "1";
+		item.style.pointerEvents = "all";
+	}
+
+	console.log(item)
+}
+
+*/
+
+
+var nav_btn = document.querySelectorAll('.nav_btn');
+function activeButton() {
+	removeActive();
+	this.classList.add('active-nav');
+}
+function removeActive() {
+	nav_btn.forEach(item => item.classList.remove('active-nav'));
+}
+nav_btn.forEach(item => item.addEventListener('click', activeButton));
+
+
+
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+		modal2.style.display = "none";
+		console.log(event.target);
+	} else if (event.target == modal2) {
+		modal2.style.display = "none";
+		modal.style.display = "none";
+	}
+}
+
+function openLogin() {
+	document.getElementById('id01').style.display='block';
+	document.getElementById('id02').style.display='none';
+}
+
+function openRegister() {
+	document.getElementById('id01').style.display='none';
+	document.getElementById('id02').style.display='block';
+}
+
+
+/* read more comment */ 
+
+function readMore(comment) {
+	var item = comment.parentNode.childNodes[3];
+	if (item.style.height === "auto") {
+		item.style.height = "44px";
+	} else {
+		item.style.height = "auto";
+	}
+}
+
+
+
+/* read more comments */
+function moreComments() {
+	var section = document.getElementById('comment-section');
+
+	if (section.style.height === "auto") {
+		section.style.height = "650px";
+	} else {
+		section.style.height = "auto";
+	}
+}
